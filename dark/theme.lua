8b8ec8d7610f28190674ed0530e4f74ce133beef
Josh Matthews <josh@jmatthews.us>
2013-04-29 17:35:35 -0400

    Using inconsolata.

--- dark/theme.lua
+++ dark/theme.lua
@@ -30,7 +30,7 @@ theme.colors.green   = "#859900ff"
 -- }}}
 
 -- {{{ Styles
-theme.font      = "Terminus 9"
+theme.font      = "Inconsolata 9"
 
 -- {{{ Colors
 theme.fg_normal  = theme.colors.base02
